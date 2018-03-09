35c89b37c3ee991423989445ccd3aa3f349cd9d7
Hudziak Paulina <paulina.hudziak@tieto.com>
2018-03-09 08:47:12 +0100

    cosmetic changes in about module and component

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { MaterialModule } from '@app/material.module';
+
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
 
