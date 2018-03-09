4681b553727411ce8651f55dd09c962c99c9d5e7
Hudziak Paulina <paulina.hudziak@tieto.com>
2018-03-09 08:47:59 +0100

    add logstore to shell and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { TranslateModule } from '@ngx-translate/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-
-import { environment } from '@env/environment';
-import { CoreModule } from '@app/core';
-import { SharedModule } from '@app/shared';
-import { HomeModule } from './home/home.module';
-import { AboutModule } from './about/about.module';
-import { XrayModule } from './xRay/xray.module';
-import { LoginModule } from './login/login.module';
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { XrayComponent } from './xray/xray.component';
-// import { GetData } from '@app/xRay/get-data.service';
+import { AboutModule } from './about/about.module';
+import { CoreModule } from '@app/core';
+import { HomeModule } from './home/home.module';
+import { LoginModule } from './login/login.module';
+import { LogstoreModule } from './logstore/logstore.module';
+import { SharedModule } from '@app/shared';
+import { XrayModule } from './xRay/xray.module';
 
+import { AppComponent } from './app.component';
+import { XrayComponent } from './xray/xray.component';
+import { LogstoreComponent } from './logstore/logstore.component';
+// import { GetData } from '@app/xRay/get-data.service';
+import { environment } from '@env/environment';
 
 
 @NgModule({
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     FormsModule,
     HttpClientModule,
     TranslateModule.forRoot(),
@@ -36,6 +37,7 @@ import { XrayComponent } from './xray/xray.component';
     AboutModule,
     LoginModule,
     XrayModule,
+    LogstoreModule,
     AppRoutingModule
   ],
   declarations: [AppComponent],
