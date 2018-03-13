db94606bdd14e66327d8b92b456bb0f2f34122a8
bcholost <atrapagb@gmail.com>
2018-03-13 13:04:38 +0100

    test part 2

--- src/app/global/get-data.service.spec.ts
+++ src/app/global/get-data.service.spec.ts
@@ -1,18 +1,18 @@
-import { TestBed, async,inject } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import {HttpClientModule} from '@angular/common/http';
-import { GetDataService } from './get-data.service';
+// import { TestBed, async,inject } from '@angular/core/testing';
+// import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+// import {HttpClientModule} from '@angular/common/http';
+// import { GetDataService } from './get-data.service';
 
-describe('GetDataService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [GetDataService, HttpClientTestingModule, HttpTestingController ],
-      imports: [HttpClientTestingModule]
-    });
-  });
+// describe('GetDataService', () => {
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       providers: [GetDataService, HttpClientTestingModule, HttpTestingController ],
+//       imports: [HttpClientTestingModule]
+//     });
+//   });
 
-  it(`should create`, async(inject([HttpTestingController, GetDataService],
-    (httpClient: HttpTestingController, apiService: GetDataService) => {
-      expect(GetDataService).toBeTruthy();
-  })));
-});
+//   it(`should create`, async(inject([HttpTestingController, GetDataService],
+//     (httpClient: HttpTestingController, apiService: GetDataService) => {
+//       expect(GetDataService).toBeTruthy();
+//   })));
+// });
--- src/app/xray/xray.component.spec.ts
+++ src/app/xray/xray.component.spec.ts
@@ -1,30 +1,142 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { FormsModule } from '@angular/forms';
+
+import { MaterialModule } from '@app/material.module';
 import { XrayComponent } from './xray.component';
 
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { GetDataService } from '@app/global/get-data.service';
+
+describe('Component: XrayComponent', () => {
+  let component: XrayComponent;
 
 
-describe('xray', () => {
-  let expected = '';
-  let notExpected = '';
-  // let expectMatch: any;
 
   beforeEach(() => {
-    expect(() => {
-      expected = 'xray';
-      notExpected = 'helllloososo';
-      // expectMatch = new RegExp(/^xray/);
+    TestBed.configureTestingModule({
+      declarations: [XrayComponent],
+      imports: [MaterialModule, FormsModule, HttpClientTestingModule],
+      providers: [GetDataService, HttpTestingController]
     });
+
+    const fixture = TestBed.createComponent(XrayComponent);
+    component = fixture.componentInstance;
   });
 
-  afterEach(() => {
-    expected = '';
-    notExpected = '';
-  });
-  it('checks if xray is xray',
-    () => expect('xray').toBe('xray'));
-  it('checks if xray is not xray',
-    () => expect('xray').not.toBe(notExpected));
-  // it('checks if xray is starts with xray',
-  //   () => expect('xray').toMatch(expectMatch));
+  it('should have a defined component', () => {
+    expect(component).toBeDefined();
+  })
 
+  it('should have light value', () => {
+    expect(component.lightValue).not.toEqual(null);
+  })
+  it('should have contrast value', () => {
+    expect(component.contrastValue).not.toEqual(null);
+  })
+  it('should have patients name', () => {
+    expect(component.patientName).not.toEqual(null);
+  })
+  it('should have button ticked', () => {
+    expect(component.blackAndWhite).toEqual(false);
+  })
+  it('should have button not ticked', () => {
+    expect(component.blackAndWhite).not.toEqual(true);
+  })
+  it("Checking if preview button loads", function () {
+    expect(document.getElementsByClassName("mat-button-wrapper")).not.toBe(null);
+  });
+  it("Checking if capture button loads", function () {
+    expect(document.getElementsByClassName("mat-raised-button")).not.toBe(null);
+  });
+  it("Checking if sliders loads", function () {
+    expect(document.getElementsByClassName("mat-slider-ticks")).not.toBe(null);
+  });
+  it("Checking if image loads", function () {
+    expect(document.getElementsByClassName("mat-card-image")).not.toBe(null);
+  });
+  it("Checking if checkbox loads", function () {
+    expect(document.getElementsByClassName("mat-checkbox-inner-container")).not.toBe(null);
+  });
+  it("Checking if input field loads", function () {
+    expect(document.getElementsByClassName(" mat-input-infix")).not.toBe(null);
+  });
+
+  it("Checking if sound plays on trigger", function () {
+    expect(component.audio).not.toBe(null);
+  });
+  it("Checking if image is being captured", function () {
+    expect(component.comingImage).not.toBe(null);
+  });
+  it("Checking if image is being captured", function () {
+    expect(component.error).not.toEqual('xRay error');
+  });
+
+
+
+  // describe('checking parmeters change', function () {
+  //   var light = component.lightValue, value = null;
+  //   beforeEach(function () {
+  //     component.lightValue = 120;
+  //     fixture.debugElement.query(By.css('#lightValue'));
+  //     light = {
+  //       changeValue: function (newValue) {
+  //         value = newValue;
+  //       }
+  //     };
+  //     spyOn(light, 'changeValue');
+  //     light.changeValue(123);
+  //     light.changeValue(1, 3);
+  //   });
+  //   it('asdas', function () {
+  //     expect(light.changeValue).toHaveBeenCalled();
+  //   })
+  // })
+  // var params = {
+  //   'lightValue': '55', 
+
+  // }
+
+  // it('gets lightValue', function() {
+  //   spyOn(component.lightValue, 'toString').and.callFake(function() {
+  //     expect(component.lightValue).not.toBe(null);
+  //   return params[component.lightValue];
+
+  //   });
+  // });
 });
+
+
+  // let trigger;
+  // beforeEach(() => {
+  //   trigger = jasmine.createSpy('event');
+  //   component();
+  // });
+
+  // it('dispatches menu.toggle event', () => {
+  //   document.addEventListener('menu.toggle', trigger);
+
+  //   const $trigger = document.querySelector('mat-raised-button');
+  //   triggerEvent($trigger, 'click');
+
+  //   expect(trigger).toHaveBeenCalled();
+  // });
+
+
+
+// checking interval, not working properly
+  // it("should have a button disabled for 10s", function () {
+  //   setTimeout(function (hideBtn) {
+  //     setTimeout(10000);
+  //   }, 1000);
+  //   setTimeout(function (titleBtn) {
+  //     expect(setTimeout).toHaveBeenCalled();
+  //     clearInterval(0);
+  //   }, 100);
+  // });
+// wait until hide wait until show
+
+// })
+
+
