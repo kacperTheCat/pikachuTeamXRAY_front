564af26db1f62d4f81fb6d832ffd4b95ff1c4912
Hudziak Paulina <paulina.hudziak@tieto.com>
2018-03-16 11:03:39 +0100

    working change machines

--- src/app/reconfig-gui/reconfig.component.ts
+++ src/app/reconfig-gui/reconfig.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RTGMachines } from '../global/address';
+import { RTGMachines, SelectMachine } from '../global/address';
 import { environment } from '@env/environment';
 import { GetDataService } from '../global/get-data.service';
 import { MatTableDataSource, MatSelect } from '@angular/material';
@@ -12,27 +12,49 @@ import { NgModule } from '@angular/core';
 })
 export class ReconfigComponent implements OnInit {
 
-  dataSource: any= [];
+  dataSource: any = [];
   error: any;
-  titleSaveButton = "Save"; 
+  titleSaveButton = "Save";
   selectedValue: number;
+  machineIndex: object;
 
   constructor(private getMachine: GetDataService) { }
-  
+
   getMachines() {
     this.getMachine.getData(RTGMachines)
       .subscribe(
         (datas: any) => {
-          this.dataSource = datas;    
+          this.dataSource = datas;
+          console.log(this.dataSource)
         },
         (error: string) => {
           this.error = error;
           console.log('Error, no data'); // need to full error handle
-        });       
+        });
   }
 
   ngOnInit() {
-     this.getMachines();
-   }
-  
+    this.getMachines();
+  }
+
+  sendMachine() {
+    if (this.selectedValue === undefined) {
+      return false // need error handle 
+    }
+    this.machineIndex = { "chosenMachineID": this.selectedValue }
+    console.log(this.machineIndex);
+    const response = this.getMachine.postData(SelectMachine, this.machineIndex)
+    .subscribe(
+      (response) =>{
+        console.log(response);
+      }
+    )
+    ;
+  }
+
+  onSubmit() {
+    this.machineIndex = { "chosenMachineID": this.selectedValue }
+    this.sendMachine();
+  }
+
 }
